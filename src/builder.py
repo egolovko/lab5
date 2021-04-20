27beaa65a5d5987a9c018489c2d742a2a207e522
egolovko <e.golovko2020@gmail.com>
2021-04-20 14:57:32 +0300

    New class for filling storage from file

---
+++ src/builder.py
@@ -0,0 +1,63 @@
+import csv
+
+class Builder:
+
+    def __init__(self):
+        self._line = None
+        self._npass = None
+        self._ngroup = None
+        self._fname = None
+        self._lname = None
+        self._patronymic = None
+        self._subject = None
+        self._score = None
+        self._total_score_100 = None
+        self._total_score_5 = None
+
+    def reset(self):
+        self._line = None
+        self._npass = None
+        self._ngroup = None
+        self._fname = None
+        self._lname = None
+        self._patronymic = None
+        self._subject = None
+        self._score = None
+        self._total_score_100 = None
+        self._total_score_5 = None
+
+    def load(self, storage, file):
+        storage.clear()
+
+        with storage as st:
+            reader = csv.reader(file, delimiter=";")
+            for row in reader:
+                if len(row) == 0:
+                    continue
+
+                self._line = row
+                self._prepare_current_line()
+                print(self._line)
+                st.load(self._npass, self._ngroup, self._fname, self._lname, self._patronymic,
+                        self._subject, self._score, self._total_score_100, self._total_score_5)
+
+    def _prepare_current_line(self):
+
+        if len(self._line) < 9:
+            raise ValueError
+
+        self._convert_fields()
+
+    def _convert_fields(self):
+        if not (self._line[3].isdigit() or self._line[4].isdigit() or self._line[6].isdigit()):
+            raise ValueError
+
+        self._ngroup = self._line[0]
+        self._subject = self._line[1]
+        self._lname = self._line[2]
+        self._total_score_100 = int(self._line[3])
+        self._score = int(self._line[4])
+        self._fname = self._line[5]
+        self._total_score_5 = int(self._line[6])
+        self._patronymic = self._line[7]
+        self._npass = self._line[8]
