059236b47764987b3a19293335debdb6fd14641b
egolovko <e.golovko2020@gmail.com>
2021-04-29 18:11:29 +0300

    Add output, bug fixed

--- src/lab5, 70.py
+++ src/lab5, 70.py
@@ -15,15 +15,21 @@ from loading_exception import LoadingException
 
 def process(init_file_path: str) -> None:
 
+    storage = Information.get_instance()
+
     print(f"ini {init_file_path}:", end=" ")
     config = loader.load_ini(init_file_path)
     print("OK")
 
     loader.load(
-        Information.get_instance,
+        storage,
         config["input"]["csv"],
         config["input"]["json"],
         config["input"]["encoding"])
+
+    print(f"output {config['output']['fname']}:", end=" ")
+    storage.output(config["output"]["fname"], config["output"]["encoding"])
+    print("OK")
 
 
 def _main(args):
