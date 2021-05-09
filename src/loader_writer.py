efc1bd45bd3d7ac26fa75a2fd8eb82f9e67cf4a2
egolovko <e.golovko2020@gmail.com>
2021-05-09 18:35:07 +0300

    Rename module, add output fucntion, bug fixed: invalid param type in docstring

--- src/loader_writer.py
+++ src/loader_writer.py
@@ -15,7 +15,7 @@ def load(storage, csv_file, json_file, encoding="utf-8"):
     csv_file : str
         Path to csv file with main data.
 
-    json_file : json
+    json_file : str
         Path to json file with statistics info.
 
     encoding : str
@@ -38,7 +38,7 @@ def load(storage, csv_file, json_file, encoding="utf-8"):
 
     print(f"json?=csv:", end=" ")
     if not fit(storage, stat):
-        raise ValueError(f"Comparation error")
+        raise ValueError(f"Comparison error")
     print("OK")
 
 
@@ -66,7 +66,7 @@ def load_ini(path):
         data = json.load(ini_file)
 
     if not _check_ini_structure(data):
-        raise ValueError(f"incorrect data format in ini file")
+        raise ValueError(f"Incorrect data format in ini file")
 
     return data
 
@@ -126,10 +126,10 @@ def load_stat(path, encoding="utf-8"):
         data = json.load(stat_file)
 
     if not _check_stat_structure(data):
-        raise ValueError(f"incorrect data format in stat file")
+        raise ValueError(f"Incorrect data format in stat file")
 
     if not _check_stat_values(data):
-        raise ValueError(f"incorrect data format in stat file")
+        raise ValueError(f"Incorrect data format in stat file")
 
     prepared_data = _prepare_stat(data)
     return prepared_data
@@ -157,6 +157,82 @@ def fit(storage, stat):
            storage.scores_100_count == stat["count_rating_100"]
 
 
+def output(storage, path, encoding="utf-8"):
+    """
+    Save prepared information in file.
+
+    Parameters
+    ----------
+    storage : information.Information
+        Storage with processed information
+
+    path : str
+        Path to file.
+
+    encoding : str
+        File encoding.
+        Default utf-8.
+    """
+    with open(path, "w", encoding=encoding) as out_file:
+        _output(storage, out_file)
+
+
+def _output(storage, file):
+    result_data = _get_subjects_list_with_max_mean(storage)
+    for info in result_data:
+        file.write(f"{info['exam'].subject}\t{info['mean']:.1f}\t{info['count_of_failed']}\n")
+        for student in info["less_than_95"]:
+            file.write(f"\t{student.lname}\t{student.fname}\t{student.patronymic}\t{student.npass}" +
+                       f"\t{student.total_score_100}\t{student.total_score_5}\n")
+
+
+def _get_subjects_list_with_max_mean(storage):
+    result_data = []
+
+    all_results = _get_all_results(storage)
+    max_mean = _get_max_mean(all_results)
+    for info in all_results:
+        if info["mean"] == max_mean:
+            result_data.append(info)
+
+    return result_data
+
+
+def _get_max_mean(all_results):
+    max_mean = all_results[0]["mean"]
+
+    for info in all_results:
+        if info["mean"] > max_mean:
+            max_mean = info["mean"]
+    return max_mean
+
+
+def _get_all_results(storage):
+    res = []
+    for exam in storage:
+        info = {
+            "exam": exam,
+            "mean": _get_subject_mean_total_score(exam),
+            "count_of_failed": _get_count_of_exam_failed(exam),
+            "less_than_95": _get_less_than_95(exam)
+        }
+        info["less_than_95"].sort(key=lambda x: x.npass)
+        res.append(info)
+    return res
+
+
+def _get_subject_mean_total_score(exam):
+    return sum(student.total_score_100 for student in exam) / len(exam)
+
+
+def _get_count_of_exam_failed(exam):
+    return sum(1 for student in exam if student.total_score_5 < 3)
+
+
+def _get_less_than_95(exam):
+    return [student for student in exam if student.total_score_100 < 95]
+
+
 def _check_ini_structure(data):
     if "input" not in data:
         return False
@@ -178,7 +254,7 @@ def _check_stat_structure(data):
     return True
 
 
-def _prepare_stat(data: object):
+def _prepare_stat(data):
     new_data = {
         "total_records_count": int(data["загальна кiлькiсть записiв"]),
         "count_rating_100": int(data["кiлькiсть оцiнок 100"])
@@ -186,7 +262,7 @@ def _prepare_stat(data: object):
     return new_data
 
 
-def _check_stat_values(data: object):
+def _check_stat_values(data):
     total_records_count = data["загальна кiлькiсть записiв"]
     count_rating_100 = data["кiлькiсть оцiнок 100"]
 
