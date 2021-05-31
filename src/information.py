0115593924b3431fbc8cba8dcdf896478c91c5fe
egolovko <e.golovko2020@gmail.com>
2021-05-31 21:52:41 +0300

    Reformat __new__

--- src/information.py
+++ src/information.py
@@ -34,7 +34,7 @@ class Information:
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = object.__new__(cls)
+            cls._instance = super().__new__(cls)
 
         return cls._instance
 
